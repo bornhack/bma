5a1353d343cbb0b5d7a7fb2423f01c9b73995fe4
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2022-09-28 23:48:56 +0200

    Added PoC colored badge for illustrating file status. Renamed upload viewclass

--- src/files/urls.py
+++ src/files/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from files.views import UploadView
+from files.views import FilesUploadView
 from files.views import FilesManageListView
 
 app_name = "files"
 
 urlpatterns = [
     path("manage/", FilesManageListView.as_view(), name="manage"),
-    path("upload/", UploadView.as_view(), name="upload"),
+    path("upload/", FilesUploadView.as_view(), name="upload"),
 ]
 
