6241fe4a8f6c7eef4d1a7ec114688a680e7ec2d8
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2022-10-07 20:38:51 +0200

    Refactored the success_message into a method

--- src/files/mixins.py
+++ src/files/mixins.py
@@ -14,20 +14,18 @@ class FilesApprovalMixin(LoginRequiredMixin):
     success_msg_postfix: str
 
     def dispatch(self, request, *args, **kwargs):
+        """Verify object has allowed status or redirect with error_msg."""
         self.object = self.get_object()
         if self.object.status in self.allowed_approval_status:
             return super().dispatch(request, *args, **kwargs)
-
         self.error_message()
         return HttpResponseRedirect(self.get_success_url())
 
     def form_valid(self, form):
+        """Update the status and return with a success_msg"""
         self.object.status = self.updated_status
         self.object.save()
-        messages.success(
-            self.request,
-            f"File {self.object.title} was {self.success_msg_postfix}",
-        )
+        self.success_message()
         return super().form_valid(form)
 
     def error_message(self):
@@ -35,4 +33,10 @@ class FilesApprovalMixin(LoginRequiredMixin):
             self.request,
             f"File '{self.object.title}' with status "
             f"'{self.object.get_status_display()}' was {self.error_msg_postfix}!",
+        )
+
+    def success_message(self):
+        messages.success(
+            self.request,
+            f"File {self.object.title} was {self.success_msg_postfix}",
         )
