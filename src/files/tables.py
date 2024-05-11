391c81fae92b590dcd1209d90c292c99905d1bef
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2024-05-11 19:55:51 +0200

    Added anchor elements to the UUID's in files table linking to the file details view

--- src/files/tables.py
+++ src/files/tables.py
@@ -1,5 +1,7 @@
 """This module defines the table used to show files."""
 import django_tables2 as tables
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import BaseFile
 
@@ -15,3 +17,8 @@ class FileTable(tables.Table):
         model = BaseFile
         template_name = "django_tables2/bootstrap.html"
         fields = ("uuid", "albums", "attribution", "uploader", "license", "file_size", "status", "title")
+
+    def render_uuid(self, value: str) -> str:
+        """Render column data with 'a' tag and return as formatted html."""
+        url = reverse("files:detail", kwargs={"pk": value})
+        return format_html(f"<a href='{url}'>{value}</a>")
