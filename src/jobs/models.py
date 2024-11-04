5544c85d3975cf806a5977fd1975ff058d0d03d5
Thomas Steen Rasmussen <tykling@bornhack.org>
2024-11-04 17:52:09 +0100

    calculate job height on server instead of leaving it to the client

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -90,39 +90,31 @@ class ImageConversionJob(BaseJob):
 
     width = models.PositiveIntegerField(help_text="The desired width of the converted image.")
 
+    height = models.PositiveIntegerField(help_text="The desired height of the converted image.")
+
     filetype = models.CharField(
         max_length=10,
         validators=[validate_image_filetype],
         help_text="The desired file type for this job.",
     )
 
-    aspect_ratio_numerator = models.PositiveIntegerField(
-        null=True,
-        blank=True,
-        help_text="The numerator part of the desired aspect ratio of the image. Leave blank to keep original AR.",
-    )
-
-    aspect_ratio_denominator = models.PositiveIntegerField(
-        null=True,
-        blank=True,
-        help_text="The denominator part of the desired aspect ratio of the image. Leave blank to keep original AR.",
+    custom_aspect_ratio = models.BooleanField(
+        default=False,
+        help_text="True if this job needs cropping to a custom AR, False if no crop is needed.",
     )
 
     @property
-    def aspect_ratio(self) -> Fraction | None:
-        """Return image AR as a Fraction."""
-        try:
-            return Fraction(self.aspect_ratio_numerator, self.aspect_ratio_denominator)
-        except TypeError:
-            return None
+    def aspect_ratio(self) -> Fraction:
+        """Return job AR as a Fraction."""
+        return Fraction(self.width, self.height)
 
     def get_result_path(self) -> tuple[Path, str]:
         """Return the path and filename for the job result."""
         orig = Path(self.basefile.original.path)
         path = orig.parent / orig.stem
-        if self.aspect_ratio:
-            path /= f"{self.aspect_ratio_numerator}_{self.aspect_ratio_denominator}"
-        # make sure the result path exists
+        if self.custom_aspect_ratio:
+            # add /4_3/ to the path for AR 4/3
+            path /= str(self.aspect_ratio).replace("/", "_")
         path.mkdir(parents=True, exist_ok=True)
         filename = f"{self.width}w.{self.filetype.lower()}"
         return path, filename
