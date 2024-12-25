8bf584c263f0b3e88dea3a3833dd0225403c034c
Rudy (zarya) <git@gigafreak.net>
2024-12-25 12:39:02 +0100

    Change f to data

--- src/static_src/js/uploadClient.js
+++ src/static_src/js/uploadClient.js
@@ -476,7 +476,7 @@ class UploadClient {
    */
   async uploadThumbnailSource(uuid, result, filename, metadata=undefined) {
     var data = new FormData()
-    data.append('f', result, filename);
+    data.append('data', result, filename);
     data.append('client', JSON.stringify({ "client_uuid": this.client_uuid, "client_version": this.client_version }))
     if (metadata) {
       data.append('metadata', JSON.stringify(metadata))
