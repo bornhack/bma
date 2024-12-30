3507a0d5958284f44a8da1196b33fa552d664bd3
Rudy (zarya) <git@gigafreak.net>
2024-12-30 20:45:45 +0100

    Added more styling to the frontpage, fixed sorting of pics on frontpage, added description to pswp caption

--- src/frontpage/views.py
+++ src/frontpage/views.py
@@ -25,6 +25,7 @@ class FrontpageTemplateView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, QuerySet[Image]]:  # noqa: ANN401
         """Add recent files to the context."""
         context = super().get_context_data(**kwargs)
+        context["6_last"] = BaseFile.bmanager.get_permitted(user=self.request.user).order_by("-created_at")[:6]
         context["6_last_images"] = self._query_last_6_uploads(self.request.user, "image")
         context["6_last_videos"] = self._query_last_6_uploads(self.request.user, "video")
         context["6_last_audios"] = self._query_last_6_uploads(self.request.user, "audio")
@@ -40,7 +41,7 @@ class FrontpageTemplateView(TemplateView):
             return (  # type: ignore[no-any-return]
                 BaseFile.bmanager.get_permitted(user=user)
                 .filter(polymorphic_ctype__model=model)
-                .order_by("created_at")[:6]
+                .order_by("-created_at")[:6]
             )
         except BaseFile.DoesNotExist:
             return None
