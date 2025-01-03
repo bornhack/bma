367f543ded9c04ce30ec12c1f4b6d32d40af9844
Rudy <387694+zarya@users.noreply.github.com>
2025-01-03 10:40:52 +0100

    Frontpage styling (#302)
    * Added more styling to the frontpage, fixed sorting of pics on frontpage, added description to pswp caption
    
    * Some more improvements to the frontpage
    
    * Show video in 16/9
    
    * That was totally unneeded
    
    * Added links to frontpage items to there files page
    
    * Use slideshow thing for audio to
    
    * Add the right column count to Audio
    
    * Show 12 pictures on wider screens
    
    * Make title text on frontpages not push stuff down
    
    * Made more columns happen, added links to the headers

--- src/frontpage/views.py
+++ src/frontpage/views.py
@@ -25,14 +25,18 @@ class FrontpageTemplateView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, QuerySet[Image]]:  # noqa: ANN401
         """Add recent files to the context."""
         context = super().get_context_data(**kwargs)
-        context["6_last_images"] = self._query_last_6_uploads(self.request.user, "image")
-        context["6_last_videos"] = self._query_last_6_uploads(self.request.user, "video")
-        context["6_last_audios"] = self._query_last_6_uploads(self.request.user, "audio")
-        context["6_last_documents"] = self._query_last_6_uploads(self.request.user, "document")
-        context["6_most_popular"] = BaseFile.bmanager.get_permitted(user=self.request.user).order_by("-hits")[:6]
+        context["last_images"] = self._query_last_uploads(self.request.user, "image")
+        context["last_videos"] = self._query_last_uploads(self.request.user, "video")
+        context["last_audios"] = self._query_last_uploads(self.request.user, "audio")
+        context["last_documents"] = self._query_last_uploads(self.request.user, "document")
+
+        context["popular_images"] = self._query_most_popular(self.request.user, "image")
+        context["popular_videos"] = self._query_most_popular(self.request.user, "video")
+        context["popular_audios"] = self._query_most_popular(self.request.user, "audio")
+        context["popular_documents"] = self._query_most_popular(self.request.user, "document")
         return context
 
-    def _query_last_6_uploads(
+    def _query_last_uploads(
         self, user: UserType | AnonymousUser, model: str
     ) -> QuerySet[Audio | Video | Image | Document] | None:
         """Get the last 6 published uploads for a model."""
@@ -40,7 +44,18 @@ class FrontpageTemplateView(TemplateView):
             return (  # type: ignore[no-any-return]
                 BaseFile.bmanager.get_permitted(user=user)
                 .filter(polymorphic_ctype__model=model)
-                .order_by("created_at")[:6]
+                .order_by("-created_at")[:12]
+            )
+        except BaseFile.DoesNotExist:
+            return None
+
+    def _query_most_popular(
+        self, user: UserType | AnonymousUser, model: str
+    ) -> QuerySet[Audio | Video | Image | Document] | None:
+        """Get the 6 most popular uploads for a model."""
+        try:
+            return (  # type: ignore[no-any-return]
+                BaseFile.bmanager.get_permitted(user=user).filter(polymorphic_ctype__model=model).order_by("-hits")[:12]
             )
         except BaseFile.DoesNotExist:
             return None
