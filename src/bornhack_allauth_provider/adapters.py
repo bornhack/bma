833c96593ba9471b14e2e812c3f18d472eb208d5
Thomas Steen Rasmussen <tykling@bornhack.org>
2025-05-04 07:50:40 +0200

    Use handle instead of username

--- src/bornhack_allauth_provider/adapters.py
+++ src/bornhack_allauth_provider/adapters.py
@@ -34,7 +34,7 @@ class BornHackSocialAccountAdapter(DefaultSocialAccountAdapter):
         user_username(sociallogin.user, data.get("username"))
 
         # set initial handle on the user object to the bornhack username
-        user_field(sociallogin.user, "handle", data["username"])
+        user_field(sociallogin.user, "handle", data["handle"])
 
         # set initial display_name on the user object to the bornhack profiles public_credit_name
         user_field(sociallogin.user, "display_name", data["public_credit_name"])
